0dffa6e1ba11f7c8f773b96091738d019c66f594
Saeejith Nair <saeejithn@gmail.com>
2022-01-25 03:00:20 -0500

    Simulate Braitenberg vehicle with 2 sensors and 2 motors allowing direct and cross-connections

--- worksheet2/braitenberg_q1/controllers/braitenberg_vehicle/braitenberg_vehicle.py
+++ worksheet2/braitenberg_q1/controllers/braitenberg_vehicle/braitenberg_vehicle.py
@@ -4,50 +4,82 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 
-TIMESTEP = 64
-MAX_SPEED = 10
 
-def run_robot(robot):
-    # Enable motors
-    wheels = []
-    wheelNames = ["wheel1","wheel2","wheel3","wheel4"]
-    
-    for i in range(len(wheelNames)):
-        wheels.append(robot.getDevice(wheelNames[i]))
-        wheels[i].setPosition(float('inf'))
-        wheels[i].setVelocity(0.0)
+class BraitenbergVehicle:
+    def __init__(self, wheel_names, sensor_names):
+        self.wheel_names = wheel_names
+        self.sensor_names = sensor_names
+        self.wheels = {}
+        self.sensors = {}
         
-    # Enable light sensors
-    light_sensors = []
-    light_sensor_names = ['ls_left', 'ls_right']
-    for i in range(len(light_sensor_names)):
-        light_sensors.append(robot.getDevice(light_sensor_names[i]))
-        light_sensors[i].enable(TIMESTEP)
-
-    # Main loop:
-    # - perform simulation steps until Webots is stopping the controller
-    while robot.step(TIMESTEP) != -1:
-        # Read the sensors:
-        left_light_val = light_sensors[0].getValue()
-        right_light_val = light_sensors[1].getValue()
-
-        # Process sensor data here.
-        left_speed = -MAX_SPEED
-        right_speed = -MAX_SPEED
-    
-        # Enter here functions to send actuator commands, like:
-        # Wheel 1,2 are on the right side
-        wheels[0].setVelocity(right_speed)
-        wheels[1].setVelocity(right_speed)
+        self.TIMESTEP = 64
+        self.MAX_SPEED = 10
         
-        # Wheel 3,4 are on the left side
-        wheels[2].setVelocity(left_speed)
-        wheels[3].setVelocity(left_speed)
+        self.initial_wheel_position = float('inf')
+        self.initial_wheel_velocity = 0.0
+        
+        # Scale factor for normalizing sensor measurements
+        self.normalization_factor = 100.0
+        
+        self.robot = Robot()
+        
+        self.setup_robot(self.wheel_names, self.sensor_names)
+        
+        self.signs = {'attract': -1.0, 'repel': 1.0}
+        
+    def setup_robot(self, wheel_names, sensor_names):        
+        # Enable motors
+        for i in range(len(wheel_names)):
+            self.wheels[wheel_names[i]] = self.robot.getDevice(wheel_names[i])
+            self.wheels[wheel_names[i]].setPosition(self.initial_wheel_position)
+            self.wheels[wheel_names[i]].setVelocity(self.initial_wheel_velocity)
+            
+        # Enable sensors
+        for i in range(len(sensor_names)):
+            self.sensors[sensor_names[i]] = self.robot.getDevice(sensor_names[i])
+            self.sensors[sensor_names[i]].enable(self.TIMESTEP)
+            
+    def transfer_function_double_connection(self, input1, input2, sign=-1.0):
+        output1 = sign*input1/self.normalization_factor
+        output2 = sign*input2/self.normalization_factor
+        
+        return (output1, output2)
+            
+    def run_robot(self, connection_type, attraction_type):
+        # Perform simulation steps until Webots is stopped by the controller
+        while self.robot.step(self.TIMESTEP) != -1:
+            # Read the sensors:
+            left_light_val = self.sensors['ls_left'].getValue()
+            right_light_val = self.sensors['ls_right'].getValue()
     
-# Enter here exit cleanup code.
+            # Process sensor data here.
+            if connection_type == 'direct':
+                left_speed, right_speed = self.transfer_function_double_connection(
+                                           left_light_val, right_light_val, self.signs[attraction_type])
+            elif connection_type == 'cross':
+                # For cross connection, swap connection sides.
+                right_speed, left_speed = self.transfer_function_double_connection(
+                                           left_light_val, right_light_val, self.signs[attraction_type])
+            else:
+                raise ValueError(f'Unsupported connection type {connection_type}')
+            
+            print(left_speed, right_speed)
+        
+            # Enter here functions to send actuator commands, like:
+            # Wheel 1 and 2 is on the right side
+            self.wheels['wheel1'].setVelocity(right_speed)
+            # self.wheels['wheel2'].setVelocity(right_speed)
+            
+            # Wheel 3,4 are on the left side
+            self.wheels['wheel3'].setVelocity(left_speed)
+            # self.wheels['wheel4'].setVelocity(left_speed)
 
 if __name__ == "__main__":
-    # create the Robot instance.
-    my_robot = Robot()
-    run_robot(my_robot)
+    # Create Braitenberg Vehicle
+    wheel_names = ["wheel1", "wheel2", "wheel3", "wheel4"]
+    wheel_names = ["wheel1", "wheel3"]
+    light_sensor_names = ['ls_left', 'ls_right']
+    braitenberg_vehicle = BraitenbergVehicle(wheel_names, light_sensor_names)
+    
+    braitenberg_vehicle.run_robot(connection_type='cross', attraction_type='repel')
     
